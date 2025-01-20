eed8b63c10e62c4f7a0303dca77bd0b83925a32b
Arpan Mondal <adbarpan@gmail.com>
2025-01-20 16:31:16 +0530

    first commit

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Logo() {
   return (
-    <img src="..\public\blogx__1_-removebg-preview.png" alt="Logo" className='scale-150 w-20 h-20'/>
+    <img src="/logo.png" alt="Logo" className='scale-150 w-20 h-20'/>
   )
 }
 
